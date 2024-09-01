5a7da53ae0625e67aa8718d0d40a514dbffb9831
kittyyy27 <118409125+kittyyy27@users.noreply.github.com>
2024-09-01 15:55:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ccaxqjxplqox.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanweisiang-employee"
+customregion = "us-east-1"
+
